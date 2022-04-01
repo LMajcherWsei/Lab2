93fc5b8e876f208180ce2723af4219dca80c19b4
Lukmajcher <100759817+LMajcherWsei@users.noreply.github.com>
2022-04-01 15:36:12 +0200

    Zadanie 3: odtworzenie programu z wykładu

--- src/main/java/com/example/lab2/HelloController.java
+++ src/main/java/com/example/lab2/HelloController.java
@@ -1,14 +1,25 @@
 package com.example.lab2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
+    public TextField text1;
+    Integer cnt = 0;
+
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void OnAction(ActionEvent actionEvent) {
+        System.out.printf("Klawisz kliknięty!");
+        text1.setText("Klawisz kliknięty po raz: " + ++cnt);
     }
 }
